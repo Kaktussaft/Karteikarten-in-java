6fcc4f593573970e6d4d3a160e716ae59d4fa3b8
Benjamin <aeplibenjamin@gmail.com>
2023-12-05 17:11:59 +0100

    Remove unnecessary things

--- FlashcardAppGUI.java
+++ FlashcardAppGUI.java
@@ -3,50 +3,37 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.util.ArrayList;
+
 public class FlashcardAppGUI extends JFrame {
     private JButton showButton;
     private JLabel flashcardLabel;
-    private JList<String> friendList;
+    private JList<Freund> friendList;
     private JTextArea friendInfo;
 
     public FlashcardAppGUI() {
         setTitle("Karteikarten für Freunde");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
-        showButton = new JButton("Show Flashcard");
-        flashcardLabel = new JLabel();
-
-        showButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Replace this with your flashcard logic
-                String flashcardText = "This is a sample flashcard text.";
-                flashcardLabel.setText(flashcardText);
-            }
-        });
-
-        String[] friends = {"John Doe", "Jane Doe"};
-        friendList = new JList<>(friends);
-        friendList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        friendList.addListSelectionListener(e -> {
-            if (!e.getValueIsAdjusting()) {
-                String selectedFriend = friendList.getSelectedValue();
-                String friendInfoText = "This is the full information of " + selectedFriend + ".";
-                friendInfo.setText(friendInfoText);
-            }
-        });
-
-        friendInfo = new JTextArea();
-        friendInfo.setEditable(false);
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(friendList), new JScrollPane(friendInfo));
-        splitPane.setDividerLocation(200);
-
-        add(showButton, BorderLayout.NORTH);
-        add(flashcardLabel, BorderLayout.SOUTH);
+    
+        // Create the friend list
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (Freund freund : Freund.Freund_Liste) {
+            listModel.addElement(freund.Vorname + " " + freund.Nachname);
+        }
+        JList<String> friendList = new JList<>(listModel);
+        JScrollPane friendListScrollPane = new JScrollPane(friendList);
+    
+        // Create the input and edit options
+        JPanel inputAndEditPanel = new JPanel();
+      
+    
+        // Create the split pane
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, friendListScrollPane, inputAndEditPanel);
+        splitPane.setDividerLocation(200); // Set the initial divider location
+    
+        // Add the split pane to the frame
         add(splitPane, BorderLayout.CENTER);
-
         setSize(1000, 700);
-        setLocationRelativeTo(null); // Centers the frame
     }
 }
