0ce04c7e38a8a864bcfde2398825c08f87a0a1d3
Benjamin <aeplibenjamin@gmail.com>
2023-12-08 21:56:30 +0100

    comments

--- FlashcardAppGUI.java
+++ FlashcardAppGUI.java
@@ -3,19 +3,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import java.util.ArrayList;
-
 public class FlashcardAppGUI extends JFrame {
-    private JButton showButton;
-    private JLabel flashcardLabel;
-    private JList<Freund> friendList;
     private JTextArea friendInfo;
 
     public FlashcardAppGUI() {
         setTitle("Karteikarten für Freunde");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-    
+
         // Create the friend list
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (Freund freund : Freund.Freund_Liste) {
@@ -23,17 +18,50 @@ public class FlashcardAppGUI extends JFrame {
         }
         JList<String> friendList = new JList<>(listModel);
         JScrollPane friendListScrollPane = new JScrollPane(friendList);
-    
-        // Create the input and edit options
-        JPanel inputAndEditPanel = new JPanel();
-      
-    
-        // Create the split pane
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, friendListScrollPane, inputAndEditPanel);
+
+        // Create the label for the friend list
+        JLabel friendListLabel = new JLabel("Alle gespeicherten Freunde");
+
+        // Create a panel to hold the label and the friend list
+        JPanel friendListPanel = new JPanel(new BorderLayout());
+        friendListPanel.add(friendListLabel, BorderLayout.NORTH);
+        friendListPanel.add(friendListScrollPane, BorderLayout.CENTER);
+
+        // Create the search field and add functionality to it
+        JTextField searchField = new JTextField(20);
+        searchField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = searchField.getText();
+                Freund freund = Kartei.suchFreund(name);
+                if (freund != null) {
+                    friendInfo.setText(freund.toString());
+                } else {
+                    friendInfo.setText("Kein Freund gefunden mit Name: " + name);
+                }
+            }
+        });
+
+        // Create the label for the search field
+        JLabel searchLabel = new JLabel("Suche Freund");
+
+        // Create the friend info area
+        friendInfo = new JTextArea();
+        friendInfo.setEditable(false);
+
+        // Create a panel to hold the search field, search label, and friend info area
+        JPanel inputAndEditPanel = new JPanel(new BorderLayout());
+        inputAndEditPanel.add(searchLabel, BorderLayout.NORTH);
+        inputAndEditPanel.add(searchField, BorderLayout.CENTER);
+        inputAndEditPanel.add(new JScrollPane(friendInfo), BorderLayout.SOUTH);
+
+        // Create the split pane and add the friend list panel and input/edit panel to it
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, friendListPanel, inputAndEditPanel);
         splitPane.setDividerLocation(200); // Set the initial divider location
-    
+
         // Add the split pane to the frame
         add(splitPane, BorderLayout.CENTER);
+
+        // Set the size of the frame
         setSize(1000, 700);
     }
 }
