1db11c6fc3a2c9c24f535643932904c322cccc6d
Benjamin <aeplibenjamin@gmail.com>
2023-12-12 16:22:49 +0100

    Refactor Main class and remove unused code

--- Main.java
+++ Main.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.SwingUtilities;
-
-public class Main extends FlashcardAppGUI {
+public class Main {
 
     Freund freund = new Freund(null, null, null, null);
     Kartei kartei = new Kartei();
@@ -30,48 +28,89 @@ public class Main extends FlashcardAppGUI {
         Option_1("Bob", "Johnson", LocalDate.of(1996, 4, 4), "Street4", "4D", "45678", "City4", "Country4");
         Option_1("Charlie", "Brown", LocalDate.of(1998, 6, 5), "Street5", "5E", "56789", "City5", "Country5");
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new FlashcardAppGUI().setVisible(true);
-            }
-        });
-
         while (running = true) {
             List<Freund> foundFriends = new ArrayList<>();// initialize foundFriends
 
-            switch (Handlungsoptionen()) {
-                case 1:
+            switch (Handlungsoptionen()) { 
+                case 1: //Freund hinzufügen
                     Get_user_input_Name();
                     Get_user_input_Adresse();
                     Option_1(temp_Vorname, temp_Nachname, temp_Geburtstag, temp_Strasse, temp_Hausnummer, temp_PLZ,
                             temp_Stadt, temp_Land);
                     break;
 
-                case 2:
-                     foundFriends = Kartei.suchFreunde(Option_2());
-                    for (Freund friend : foundFriends) {
-                        System.out.println(friend);
-                    }
-                    break;
+                case 2: //Freund ändern
+                foundFriends = Kartei.suchFreunde(Option_2());
+                var counter = 1;
+                for (Freund freund : foundFriends) {
+                    System.out.println(counter + " " + freund.Vorname + " " + freund.Nachname);
+                    counter++;
+                }
+                var to_be_changed = Integer.parseInt(promptForNonEmptyInput("Wählen Sie aus welchen Freund Sie ändern möchten"));
+                var Requested_change = Integer.parseInt(promptForNonEmptyInput("Es gibt 3 Optionen: Das bestehende Freund Object Ändern: 1; oder eine Adresse hinzufügen 2; oder alle Adressen löschen 3"));
+                System.out.println("Falls sie ein Feld nicht ändern möchten geben Sie 0 ein");
 
-                case 3:
-                    System.out.println("Füge einen Freund hinzu: 1");
-                    break;
+                switch (Requested_change) {
+                    case 1: //Freund ändern
+                        Get_user_input_Name();
+                        Get_user_input_Adresse();
+                        Kartei.Freund_ändern(foundFriends.get(to_be_changed - 1), temp_Vorname, temp_Nachname, temp_Geburtstag, temp_Strasse, temp_Hausnummer, temp_PLZ, temp_Stadt, temp_Land);
+                        break;
+                    case 2: //Adresse hinzufügen
+                       Get_user_input_Adresse();
+                        Adresse newAdresse = new Adresse(temp_Strasse, temp_Hausnummer, temp_PLZ, temp_Stadt, temp_Land);
+                        foundFriends.get(to_be_changed - 1).Adressen.add(newAdresse);
+                        break;
+                    case 3: //Alle Adressen löschen
+                        foundFriends.get(to_be_changed - 1).Adressen.clear();
+                        break;
+                        }
 
-                case 4:
+                break;
+
+                case 3: //Freund löschen
+                    System.out.println("\n");
                     foundFriends = Kartei.suchFreunde(Option_2());
-                    for (Freund friend : foundFriends) {
-                        System.out.println(friend);
+                    var counter_delete = 1;
+                    for (Freund Freund : foundFriends) {
+                        System.out.println(counter_delete + " " + Freund.Vorname + " " + Freund.Nachname);
+                        counter_delete++;
+                    }
+                    var to_be_deleted = Integer.parseInt(promptForNonEmptyInput("Wählen Sie aus welchen Freund Sie löschen möchten"));
+                    
+                    Freund toRemove = foundFriends.get(to_be_deleted - 1);
+                    Freund.Freund_Liste.remove(toRemove);
+                
+                    System.out.println("\n");
+                    break;
+
+                case 4: //Freund suchen
+                    System.out.println("\n");
+                    foundFriends = Kartei.suchFreunde(Option_2());
+                    for (Freund Freund : foundFriends) {
+                        System.out.println(Freund.Vorname + " " + Freund.Nachname + " " + Freund.Geburtsdatum);
+
+                        Print_Out_Adresses(Freund);
+                        System.out.println("\n");
                     }
                     break;
 
-                case 5:
-                    System.out.println("Füge einen Freund hinzu: 1");
+                    case 5: //Alle Freunde anzeigen
+                    System.out.println("\n");
+                    System.out.println("Anzahl Freunde: " + Freund.Freund_Liste.size() );
+                    for (Freund freund : Freund.Freund_Liste) {
+                        System.out.println(freund.Vorname + " " + freund.Nachname + " " + freund.Geburtsdatum);
+
+                        Print_Out_Adresses(freund);
+                        System.out.println("\n");
+                    }
                     break;
             }
         }
 
     }
+
+    
 
     public static int Handlungsoptionen() {
 
@@ -107,15 +146,12 @@ public class Main extends FlashcardAppGUI {
     }
 
     public static String Option_2() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte geben Sie den Vor- oder Nachnamen des Freundes ein:");
-        String name = scanner.nextLine();
-        return name;
+        
+        return promptForNonEmptyInput("Bitte geben Sie den Namen des Freundes ein:");
     }
+    
 
-    // public static Freund Option_3() {
-    // new Freund(null, null, null, null);
-    // }
+    
 
     public static String Option_4() {
         Scanner scanner = new Scanner(System.in);
@@ -124,6 +160,7 @@ public class Main extends FlashcardAppGUI {
         return name;
     }
 
+    //make sure the user does not enter an empty string
     public static String promptForNonEmptyInput(String prompt) { // check if input is empty
         Scanner scanner = new Scanner(System.in);
         String input = "";
@@ -137,6 +174,7 @@ public class Main extends FlashcardAppGUI {
         return input;
     }
 
+    //make sure the user enters a date in the correct format
     public static LocalDate promptForDate(String prompt) { // check if date is in correct format and not null
         LocalDate date = null;
         while (date == null) {
@@ -151,6 +189,7 @@ public class Main extends FlashcardAppGUI {
         return date;
     }
 
+        //get user input for name
     public static void Get_user_input_Name() {
 
         temp_Vorname = promptForNonEmptyInput("Bitte geben Sie den Vornamen ein:");
@@ -158,12 +197,22 @@ public class Main extends FlashcardAppGUI {
         temp_Geburtstag = promptForDate("Bitte geben Sie das Geburtsdatum ein: 'dd.MM.yyyy'");
     }
 
+    //get user input for address
     public static void Get_user_input_Adresse() {
         temp_Strasse = promptForNonEmptyInput("Bitte geben Sie die Straße ein:");
         temp_Hausnummer = promptForNonEmptyInput("Bitte geben Sie die Hausnummer ein:");
         temp_PLZ = promptForNonEmptyInput("Bitte geben Sie die Postleitzahl ein:");
         temp_Stadt = promptForNonEmptyInput("Bitte geben Sie die Stadt ein:");
         temp_Land = promptForNonEmptyInput("Bitte geben Sie das Land ein:");
+    }
+
+    public static void Print_Out_Adresses(Freund freund) { //print out multiple adresses and don't throw and error if there are none
+        if(freund.Adressen.size() > 0){
+            for (Adresse adresse : freund.Adressen){
+                System.out.println(adresse.Strasse + " " + adresse.Hausnummer
+                        + ", " + adresse.Stadt + ", " + adresse.Land);
+            }
+        }
     }
 
 }
